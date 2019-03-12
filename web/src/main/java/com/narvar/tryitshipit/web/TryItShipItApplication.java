e52a759d3d858b813d2e0364c5dfb305f6f32656
VijayBhore <vijay@narvar.com>
2019-03-11 19:04:10 -0700

    Fix the application structure and pom dependencies

--- web/src/main/java/com/narvar/tryitshipit/web/TryItShipItApplication.java
+++ web/src/main/java/com/narvar/tryitshipit/web/TryItShipItApplication.java
@@ -1,9 +1,13 @@
 package com.narvar.tryitshipit.web;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackages = "com.narvar.tryitshipit.web.persistence")
 public class TryItShipItApplication {
+    public static void main(final String[] args) {
+        SpringApplication.run(TryItShipItApplication.class, args);
+    }
 }
